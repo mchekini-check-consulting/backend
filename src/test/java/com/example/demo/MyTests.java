2dddc21480b635a5450141cb1512324907a4c56e
mchekini <me.chekini@gmail.com>
2025-09-04 02:48:52 +0200

    Update Jenkinsfile

---
+++ src/test/java/com/example/demo/MyTests.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTests {
+
+
+
+    @Test
+    public void itShouldWork(){
+        assertEquals(2,3);
+    }
+}
